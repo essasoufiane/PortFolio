b45d65a75c41afba67ff164ff07f13f51afceb4f
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-04-27 13:53:42 +0200

    integrartion_structure

---
+++ index.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    <title>Drive-X</title>
+</head>
+<body>
+    
+    <?php include_once "partials/header.php";?>
+
+    <div class="container">
+    <div class="jumbotron my-5 text-center">
+            <h1 class="display-3 text-danger">
+                Drive-X
+            </h1>
+            <p class="lead">This is a simple hero unit, a simple jumbotron-style component for calling extra attention
+                to
+                featured content or information.</p>
+            <hr class="my-4">
+            <p>It uses utility classes for typography and spacing to space content out within the larger container.</p>
+            <p class="lead">
+                <a class="btn btn-primary btn-lg" routerLink="cars" role="button">eXotic Cars</a>
+            </p>
+        </div>
+    </div>
+
+
+    
+    <?php include_once "partials/footer.php";?>
+
+</body>
+</html>
+
+
+
+
+<!--  -->
