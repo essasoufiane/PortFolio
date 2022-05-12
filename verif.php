2a09baa061161bc4210bc8a46798fafd0dbcd555
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-05-12 16:49:16 +0200

    msg_de_connexion_reussi
    message de login success

--- verif.php
+++ verif.php
@@ -19,11 +19,32 @@ $req->closeCursor(); // coupe la connection avec la bdd
 
 $hash = $result[0]['mdp'];
 // var_dump($hash);
-echo password_verify($mdp, $hash);
+// echo password_verify($mdp, $hash);
 
 if (password_verify($mdp, $hash)) {
-    var_dump($result);
-    # code...
+    ?>
+    <!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+    <?php require_once "./partials/header.php" ?>
+    <div class="container d-flex flex-column text-center">
+        <div class="pt-5">
+
+            <p class="badge badge-success">Bien jouer tu t'es connecter !</p>
+        </div>
+
+        
+    </div>
+
+    </body>
+    </html>
+    <?php
 }else {
     ?>
     <!DOCTYPE html>
