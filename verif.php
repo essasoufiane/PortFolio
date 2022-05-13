75180ccdcd5d99e1f456047dd691b7c75055964f
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-05-13 03:03:48 +0200

    verif_email_POO_msg_erreur_email
    verification email avec pregmatch dans un object + msg erreur email ou mdp ou msg de bienvue si les données utlisateur sont exact

--- verif.php
+++ verif.php
@@ -2,22 +2,49 @@
 
 // var_dump($_POST);
 
-$mdp = $_POST['mdp'];
+
 $bdd = new PDO('mysql:host=localhost;dbname=gta_rp','root',''); // connexion à la BDD
 
 $email = $_POST["email"]; 
+$mdp = $_POST["mdp"]; 
 
-$req  = $bdd->prepare("SELECT * FROM utilisateurs WHERE email='".$email."'"); // la requete de comparaison
+// --object verification d'email-----
+class verification{
+    public $email;
+    public $emailV;
+    
+    function __construct($email)
+    {
+        $regex = "/^([a-zA-Z0-9\.]+@+[a-zA-Z]+(\.)+[a-zA-Z]{2,3})$/";
+        $this->emailV = preg_match($regex, $email) ;
+        
+    }
 
-$req->execute(); // envoi et execution en BDD 
+}
+
+// instensification de l'object 
+$x = new verification($email);
 
 
-$result = $req->fetchAll(PDO::FETCH_ASSOC); // recupere les datas de la req on met dans un tableau
 
-$req->closeCursor(); // coupe la connection avec la bdd 
+// var_dump($x->emailV);
 
 
-$hash = $result[0]['mdp'];
+if ($x->emailV) {
+    // echo 'youhou';
+    $req  = $bdd->prepare("SELECT * FROM utilisateurs WHERE email='".$email."'"); // la requete de comparaison
+    $req->execute(); // envoi et execution en BDD 
+    
+    
+    $result = $req->fetchAll(PDO::FETCH_ASSOC); // recupere les datas de la req on met dans un tableau
+    
+    $req->closeCursor(); // coupe la connection avec la bdd 
+    
+    
+    $hash = $result[0]['mdp'];
+
+
+
 // var_dump($hash);
 // echo password_verify($mdp, $hash);
 
@@ -36,7 +63,7 @@ if (password_verify($mdp, $hash)) {
     <div class="container d-flex flex-column text-center">
         <div class="pt-5">
 
-            <p class="badge badge-success">Bien jouer tu t'es connecter !</p>
+            <p class="badge badge-success">Bien jouer t'es connecter !</p>
         </div>
 
         
@@ -45,6 +72,7 @@ if (password_verify($mdp, $hash)) {
     </body>
     </html>
     <?php
+
 }else {
     ?>
     <!DOCTYPE html>
@@ -60,7 +88,34 @@ if (password_verify($mdp, $hash)) {
     <div class="container d-flex flex-column text-center">
         <div class="pt-5">
 
-            <p class="badge badge-danger">verifie ton adresse mail ou ton mots de passe</p>
+            <p class="badge badge-danger">verifie ton mots de passe</p>
+        </div>
+
+        <a class="pt-3" href="connexion.php"><button class="btn btn-primary">réessayer</button></a>
+        
+    </div>
+
+    </body>
+    </html>
+    <?php
+    
+}
+}else {
+    ?>
+    <!DOCTYPE html>
+    <html lang="fr">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Document</title>
+    </head>
+    <body>
+    <?php require_once "./partials/header.php" ?>
+    <div class="container d-flex flex-column text-center">
+        <div class="pt-5">
+
+            <p class="badge badge-danger">verifie ton adresse email</p>
         </div>
 
         <a class="pt-3" href="connexion.php"><button class="btn btn-primary">réessayer</button></a>
