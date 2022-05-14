d0dc717182b1fdfadcc56b0806ace32f206ddbe8
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-05-14 23:43:55 +0200

    texteUrgent_marquee

--- index.php
+++ index.php
@@ -1,15 +1,31 @@
 <?php require_once "partials/header.php" ?>
-<div style=" background-attachment: fixed, scroll; background-image: url('assets/images/network-4894813_1920.jpg');
-    min-height: 350px;
-    text-decoration: none;" class="banniere d-flex flex-column">
-    <h1 class="pt-5 pb-2 text-center text-white">
-        Développeur Web et Web mobile
-    </h1>
-    <h2 class="p-5 text-center text-white">
-        Soufiane Essaouari
-    </h2>
-    <div class="d-flex justify-content-center pb-3">
-
-        <button type="button" class=" btn btn-outline-light"><a href="#">Mon porifl GitHub</a></button>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    
+    <div style=" background-attachment: fixed, scroll; background-image: url('assets/images/network-4894813_1920.jpg');
+        min-height: 350px;
+        text-decoration: none;" class="banniere d-flex flex-column">
+        <h1 class="pt-5 pb-2 text-center text-white">
+            Développeur Web et Web mobile
+        </h1>
+        <h2 class="p-5 text-center text-white">
+            Soufiane Essaouari
+        </h2>
+        <div class="d-flex justify-content-center pb-3">
+    
+            <button type="button" class=" btn btn-outline-light"><a href="#">Mon porifl GitHub</a></button>
+        </div>
     </div>
-</div>
+    
+    <div class="wrapper">
+    <marquee behavior="alternate"><span class="text-danger">Urgent ! Je cherche un stage !</span></marquee>
+    </div>
+</body>
+</html>
