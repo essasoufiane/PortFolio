7b1963769c14544f7b23ab8b4ca874a33b1c4a73
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-05-15 02:47:47 +0200

    fin_presentation

--- index.php
+++ index.php
@@ -1,17 +1,20 @@
 <?php require_once "partials/header.php" ?>
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
-    <div style=" background-attachment: fixed, scroll; background-image: url('assets/images/network-4894813_1920.jpg');
+    <!-- section pricincpal -->
+    <main style=" background-attachment: fixed, scroll; background-image: url('assets/images/network-4894813_1920.jpg');
         min-height: 350px;
-        text-decoration: none;" class="banniere d-flex flex-column">
+        text-decoration: none;">
+    <section class="banniere d-flex flex-column">
         <h1 class="pt-5 pb-2 text-center text-white">
             Développeur Web et Web mobile
         </h1>
@@ -19,13 +22,45 @@
             Soufiane Essaouari
         </h2>
         <div class="d-flex justify-content-center pb-3">
-    
+
             <button type="button" class=" btn btn-outline-light"><a href="#">Mon porifl GitHub</a></button>
         </div>
-    </div>
+    </section>
+    <!-- Le texte urgence stage -->
+    <section>
+        <div class="wrapper">
+            <marquee behavior="alternate"><span class="font-weight-bolder text-danger">Urgent ! Je cherche un stage !</span></marquee>
+        </div>
+        <!-- section Présentation  -->
+        <!-- <section> -->
+            <!-- <h2>Présentation</h2>
+        <P>
+            Je suis étudiant Développeur Web et Web mobile chez webforce3 à Mérignac, je termine la formation le 20 juin et je cherche un stage pour 2 mois ou plus à partir du 20 juin sur bordeaux et ses alentours.
+        </P> -->
+            <div class="mx-auto card text-white bg-info mb-3" style="max-width: 18rem;">
+                <div class="card-header font-weight-bold">Présentation</div>
+                <div class="card-body">
+                    <!-- <h5 class="card-title">Info card title</h5> -->
+                    <p class="card-text"> Je suis étudiant Développeur Web et Web mobile chez webforce3 à Mérignac, je termine la formation le 20 juin et je cherche un stage pour 2 mois ou plus à partir du 20 juin sur bordeaux et ses alentours.</p>
+                </div>
+            </div>
+            <!-- <div class="card">
+            <h5 class="text-center card-header">Présentation</h5>
+            <div class="card-body">
+                <h5 class="card-title">Special title treatment</h5>
+                <!-- <p class="card-text"> Je suis étudiant Développeur Web et Web mobile chez webforce3 à Mérignac, je termine la formation le 20 juin et je cherche un stage pour 2 mois ou plus à partir du 20 juin sur bordeaux et ses alentours.</p>
+                <a href="#" class="btn btn-primary">Go somewhere</a> -->
+            <!-- </div> -->
+            <!-- </div> --> -->
+        <!-- </section> -->
     
-    <div class="wrapper">
-    <marquee behavior="alternate"><span class="text-danger">Urgent ! Je cherche un stage !</span></marquee>
-    </div>
+    <!-- section compétences -->
+
+    
+
+
+    </section>
+    </main>
 </body>
+
 </html>
