495419ff372033f47f8b9b401000f5f9ff383022
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-05-19 12:05:25 +0200

    anim JS
    animation JS article

--- index.php
+++ index.php
@@ -5,12 +5,12 @@
 <html lang="fr">
 
 <head>
-<html prefix="og: http://ogp.me/ns#">
-<meta property="og:title" content="Développeur Web/Mobile & Soufiane Essaouari"/>
-<meta property="og:description" content="PortFolio Développeur Soufiane Essaouari & Web/Mobile"/>
-<meta property="og:image" content="https://soufiane-essaouari.fr/assets/images/bannière-portfolio.jpg" />
-<meta property="og:url" content="https://soufiane-essaouari.fr/" />
-<meta property="og:type" content="website">
+    <html prefix="og: http://ogp.me/ns#">
+    <meta property="og:title" content="Développeur Web/Mobile & Soufiane Essaouari" />
+    <meta property="og:description" content="PortFolio Développeur Soufiane Essaouari & Web/Mobile" />
+    <meta property="og:image" content="https://soufiane-essaouari.fr/assets/images/bannière-portfolio.jpg" />
+    <meta property="og:url" content="https://soufiane-essaouari.fr/" />
+    <meta property="og:type" content="website">
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
@@ -42,7 +42,7 @@
                 </div>
             </section>
             <!------------------- Le texte urgence stage --------------->
-            
+
             <section class="pb-5">
                 <div class="wrapper">
                     <marquee behavior="alternate"><span class="font-weight-bolder text-danger hvr-pulse">Urgent ! Je cherche un stage !</span></marquee>
@@ -85,75 +85,75 @@
         </section>
         <!-- ------------------------articles 1 ------------------ -->
         <section class="d-flex flex-wrap justify-content-around">
-        <article class="py-4 hvr-grow">
-            <div class="card shadow" style="width: 18rem;">
-                <img class="card-img-top" src="assets/images/poo_mvc_fromscratchCapture.png" alt="image site php">
-                <div class="card-body">
-                    <h5 class="card-title">POO MVC fromScratch</h5>
-                    <p class="card-text">J'ai coder ce site avec une approche conceptuelle orienté objet et une architecture MVC totalement fromscratch (à la main). Cliquez <a href="https://github.com/essasoufiane/siteWeb_PourVTC_en_POO_MVC">ici</a> pour voir le code sur GitHub, et sur le bouton ci dessous pour voir le site et le tester.</p>
-                    <a href="MVC-POO-PHP-FromScratch-main/index.php" class="btn btn-primary hvr-grow">Voir le site</a>
+            <article data-aos-duration="3000" data-aos="fade-up" class="py-4">
+                <div class="card shadow" style="width: 18rem;">
+                    <img class="card-img-top" src="assets/images/poo_mvc_fromscratchCapture.png" alt="image site php">
+                    <div class="card-body">
+                        <h5 class="card-title">POO MVC fromScratch</h5>
+                        <p class="card-text">J'ai coder ce site avec une approche conceptuelle orienté objet et une architecture MVC totalement fromscratch (à la main). Cliquez <a href="https://github.com/essasoufiane/siteWeb_PourVTC_en_POO_MVC">ici</a> pour voir le code sur GitHub, et sur le bouton ci dessous pour voir le site et le tester.</p>
+                        <a href="MVC-POO-PHP-FromScratch-main/index.php" class="btn btn-primary hvr-grow">Voir le site</a>
+                    </div>
                 </div>
-            </div>
 
-        </article>
-        <article class="py-4 hvr-grow">
-            <div class="card shadow" style="width: 18rem;">
-                <img class="card-img-top" src="assets/images/screen-alpha-series.jpg" alt="image blog apha series">
-                <div class="card-body">
-                    <h5 class="card-title">Alpha Séries</h5>
-                    <p class="card-text">J'ai coder ce site avec HTML/CSS en from scratch, vous pouvez le voir sur GitHub.</p>
-                    <a href="https://essasoufiane.github.io/alpha-series/" class="btn btn-primary hvr-grow">Voir le site</a>
+            </article>
+            <article data-aos-duration="3000" data-aos="fade-up" class="py-4">
+                <div class="card shadow" style="width: 18rem;">
+                    <img class="card-img-top" src="assets/images/screen-alpha-series.jpg" alt="image blog apha series">
+                    <div class="card-body">
+                        <h5 class="card-title">Alpha Séries</h5>
+                        <p class="card-text">J'ai coder ce site avec HTML/CSS en from scratch, vous pouvez le voir sur GitHub.</p>
+                        <a href="https://essasoufiane.github.io/alpha-series/" class="btn btn-primary hvr-grow">Voir le site</a>
+                    </div>
                 </div>
-            </div>
 
-        </article>
-        <article class="py-4 hvr-grow">
-            <div class="card shadow" style="width: 18rem;">
-                <img class="card-img-top" src="assets/images/cvCapture.png" alt="image blog cryptomonnaie">
-                <div class="card-body">
-                    <h5 class="card-title">CV</h5>
-                    <p class="card-text">Vous pouvez telecharger mon CV en pdf ici.</p>
-                    <a href="assets/ESSAOUARI_Soufiane.pdf" download="ESSAOUARI_Soufiane.pdf" class="btn btn-primary hvr-grow">Telecharger le CV</a>
+            </article>
+            <article data-aos-duration="3000" data-aos="fade-up" class="py-4">
+                <div class="card shadow" style="width: 18rem;">
+                    <img class="card-img-top" src="assets/images/cvCapture.png" alt="image blog cryptomonnaie">
+                    <div class="card-body">
+                        <h5 class="card-title">CV</h5>
+                        <p class="card-text">Vous pouvez telecharger mon CV en pdf ici.</p>
+                        <a href="assets/ESSAOUARI_Soufiane.pdf" download="ESSAOUARI_Soufiane.pdf" class="btn btn-primary hvr-grow">Telecharger le CV</a>
 
+                    </div>
                 </div>
-            </div>
 
-        </article>
-        </article>
-        <article class="py-4 hvr-grow">
-            <div class="card shadow" style="width: 18rem;">
-                <img class="card-img-top" src="assets/images/cryptocurrency-6601591.jpg" alt="image blog cryptomonnaie">
-                <div class="card-body">
-                    <h5 class="card-title">CryptoManiax</h5>
-                    <p class="card-text">J'ai créé ce blog qui traite les sujets cryptomonnaies et blockchain avec WordPress.</p>
-                    <a href="https://cryptomaniax.fr/" class="btn btn-primary hvr-grow">Voir le site</a>
+            </article>
+
+            <article data-aos-duration="3000" data-aos="fade-up" class="py-4">
+                <div class="card shadow" style="width: 18rem;">
+                    <img class="card-img-top" src="assets/images/cryptocurrency-6601591.jpg" alt="image blog cryptomonnaie">
+                    <div class="card-body">
+                        <h5 class="card-title">CryptoManiax</h5>
+                        <p class="card-text">J'ai créé ce blog qui traite les sujets cryptomonnaies et blockchain avec WordPress.</p>
+                        <a href="https://cryptomaniax.fr/" class="btn btn-primary hvr-grow">Voir le site</a>
+                    </div>
                 </div>
-            </div>
 
-        </article>
-        <article class="py-4 hvr-grow">
-            <div class="card shadow" style="width: 18rem;">
-                <img class="card-img-top" src="assets/images/enregistrementImage.png" alt="image formulaire">
-                <div class="card-body">
-                    <h5 class="card-title">formulaire login</h5>
-                    <p class="card-text">J'ai créé un système de login vous pouvez vous enregistrer et vous connecter et vous  serais enregister dans la base de données de ce site vous serais afficher dans la page <a href="utilisateurs.php">utilisateurs</a> .</p>
-                    <a href="enregistrement.php" class="btn btn-primary hvr-grow">S'enregistrer</a>
+            </article>
+            <article data-aos-duration="3000" data-aos="fade-up" class="py-4">
+                <div class="card shadow" style="width: 18rem;">
+                    <img class="card-img-top" src="assets/images/enregistrementImage.png" alt="image formulaire">
+                    <div class="card-body">
+                        <h5 class="card-title">formulaire login</h5>
+                        <p class="card-text">J'ai créé un système de login vous pouvez vous enregistrer et vous connecter et vous serais enregister dans la base de données de ce site vous serais afficher dans la page <a href="utilisateurs.php">utilisateurs</a> .</p>
+                        <a href="enregistrement.php" class="btn btn-primary hvr-grow">S'enregistrer</a>
+                    </div>
                 </div>
-            </div>
 
-        </article>
-        <article class="py-4 hvr-grow">
-            <div class="card shadow" style="width: 18rem;">
-                <img class="card-img-top" src="assets/images/PortfolioCapture.png" alt="Portfolio">
-                <div class="card-body">
-                    <h5 class="card-title">Portfolio</h5>
-                    <p class="card-text">J'ai coder ce site from scratch avec PHP , vous pouvez voir le code sur GitHub.</p>
-                    <a href="https://github.com/essasoufiane/PortFolio" class="btn btn-primary hvr-grow">Voir le code</a>
+            </article>
+            <article data-aos-duration="3000" data-aos="fade-up" class="py-4">
+                <div class="card shadow" style="width: 18rem;">
+                    <img class="card-img-top" src="assets/images/PortfolioCapture.png" alt="Portfolio">
+                    <div class="card-body">
+                        <h5 class="card-title">Portfolio</h5>
+                        <p class="card-text">J'ai coder ce site from scratch avec PHP , vous pouvez voir le code sur GitHub.</p>
+                        <a href="https://github.com/essasoufiane/PortFolio" class="btn btn-primary hvr-grow">Voir le code</a>
+                    </div>
                 </div>
-            </div>
 
-        </article>
-    </section>
+            </article>
+        </section>
     </main>
 
 </body>
