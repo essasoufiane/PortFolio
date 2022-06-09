6ecfbb2835c4b62dc66863c72db24a7eb24ee079
Soufiane Essaouari <soufiane.pro@yahoo.com>
2022-06-09 19:13:48 +0200

    MAJ

--- index.php
+++ index.php
@@ -16,52 +16,56 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/css/bootstrap.min.css">
     <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.8.1/css/all.css">
+    <link rel="stylesheet" href="./css/styles.css">
     <title>Essaouari Soufiane</title>
 </head>
 
 <body>
     <!-- ---------------section pricincpal------------------ -->
     <main>
-        <div style=" background-attachment: fixed, scroll; background-image: url('assets/images/network-4894813_1920.jpg');
-        min-height: 350px;
-        text-decoration: none;">
+        <div style="background: #bfcaff;
+background: -moz-linear-gradient(top, #bfcaff 0%, #ffffff 100%);
+background: -webkit-linear-gradient(top, #bfcaff 0%, #ffffff 100%);
+background: linear-gradient(to bottom, #bfcaff 0%, #ffffff 100%);
+filter: progid:DXImageTransform.Microsoft.gradient( startColorstr='#bfcaff', endColorstr='#ffffff',GradientType=0 );
+                                ">
             <section class="banniere d-flex flex-column">
-                <h1 class="pt-5 pb-2 text-center text-white">
+                <h1 class="pt-5 pb-2 text-center clr-principale">
                     <span class="hvr-grow-rotate">
                         Développeur Web et Web mobile
                     </span>
                 </h1>
-                <h2 class="p-5 text-center text-white">
+                <h2 class="p-5 text-center clr-principale">
                     <span class="hvr-grow-rotate">
                         Soufiane Essaouari
                     </span>
                 </h2>
                 <div class="d-flex justify-content-center pb-3">
 
-                    <button type="button" class=" btn btn-outline-light"><a class="text-white" href="#">Mon porifl GitHub</a></button>
+                    <button type="button" class=" btn btn-outline-info"><a class=" clr-principale" href="https://github.com/essasoufiane">Mon profil GitHub</a></button>
                 </div>
             </section>
             <!------------------- Le texte urgence stage --------------->
 
             <section class="pb-5">
                 <div class="wrapper">
-                    <marquee behavior="alternate"><span class="font-weight-bolder text-danger hvr-pulse">Urgent ! Je cherche un stage !</span></marquee>
+                    <marquee behavior="alternate"><span class="font-weight-bolder text-danger hvr-pulse">Un devis pour votre projet ? Contactez moi !</span></marquee>
                 </div>
                 <!----------------- section Présentation ----------- -->
-                <div class="mx-auto card text-white bg-info" style="max-width: 18rem;">
+                <!-- <div class="mx-auto card text-white bg-info" style="max-width: 18rem;">
                     <div class="card-header font-weight-bold text-center">Présentation</div>
-                    <div class="card-body">
+                    <div class="card-body"> -->
                         <!-- <h5 class="card-title">Info card title</h5> -->
-                        <p class="card-text"> Je suis étudiant Développeur Web et Web mobile chez webforce3 à Mérignac, je termine la formation le 20 juin et je cherche un stage pour 2 mois ou plus à partir du 20 juin sur bordeaux et ses alentours.</p>
+                        <!-- <p class="card-text"> Je suis étudiant Développeur Web et Web mobile chez webforce3 à Mérignac, je termine la formation le 20 juin et je cherche un stage pour 2 mois ou plus à partir du 20 juin sur bordeaux et ses alentours.</p>
                     </div>
-                </div>
+                </div> -->
 
             </section>
         </div>
         <!-- ----------------section compétences------------ -->
 
         <section>
-            <h2 class="text-center list-group-item-info list-group-item list-group-item-action">Compétences</h2>
+            <h2 class=" text-center list-group-item list-group-item list-group-item-action"><span>Compétences</span></h2>
             <div class="d-flex justify-content-around flex-wrap font-weight-bold">
                 <ul>
                     <li>Angular 2+</li>
@@ -83,6 +87,11 @@
                 </ul>
             </div>
         </section>
+        <!-- <div class="card">
+        Magic Card
+      </div>
+      
+      <a href="#" target="_blank">Soufiane</a> -->
         <!-- ------------------------articles 1 ------------------ -->
         <section class="d-flex flex-wrap justify-content-around">
             <article data-aos-duration="3000" data-aos="fade-up" class="py-4">
@@ -90,8 +99,8 @@
                     <img class="card-img-top" src="assets/images/poo_mvc_fromscratchCapture.png" alt="image site php">
                     <div class="card-body">
                         <h5 class="card-title">POO MVC fromScratch</h5>
-                        <p class="card-text">J'ai coder ce site avec une approche conceptuelle orienté objet et une architecture MVC totalement fromscratch (à la main). Cliquez <a href="https://github.com/essasoufiane/siteWeb_PourVTC_en_POO_MVC">ici</a> pour voir le code sur GitHub, et sur le bouton ci dessous pour voir le site et le tester.</p>
-                        <a href="MVC-POO-PHP-FromScratch-main/index.php" class="btn btn-primary hvr-grow">Voir le site</a>
+                        <p class="card-text">J'ai coder ce site avec une approche conceptuelle orienté objet et une architecture MVC totalement fromscratch (à la main). Cliquez <a href="https://github.com/essasoufiane/siteWeb_PourVTC_en_POO_MVC">ici</a> pour voir le code sur GitHub.</p>
+                        <a href="https://github.com/essasoufiane/siteWeb_PourVTC_en_POO_MVC" class="btn btn-primary hvr-grow">Voir le site</a>
                     </div>
                 </div>
 
@@ -136,7 +145,7 @@
                     <img class="card-img-top" src="assets/images/enregistrementImage.png" alt="image formulaire">
                     <div class="card-body">
                         <h5 class="card-title">formulaire login</h5>
-                        <p class="card-text">J'ai créé un système de login vous pouvez vous enregistrer et vous connecter et vous serais enregister dans la base de données de ce site vous serais afficher dans la page <a href="utilisateurs.php">utilisateurs</a> .</p>
+                        <p class="card-text">J'ai créé un système de login from scratch vous pouvez vous enregistrer et vous connecter et vous serais enregister dans la base de données de ce site vous serais afficher dans la page <a href="utilisateurs.php">utilisateurs</a> .</p>
                         <a href="enregistrement.php" class="btn btn-primary hvr-grow">S'enregistrer</a>
                     </div>
                 </div>
--- partials/header.php
+++ partials/header.php
@@ -20,8 +20,8 @@
 
 <body>
 
-    <nav class="navbar navbar-expand-lg navbar-light bg-info">
-        <a class="navbar-brand font-weight-bold hvr-grow text-white" href="index.php">Acceuil</a>
+    <nav class="navbar navbar-expand-lg">
+        <a class="navbar-brand font-weight-bold hvr-grow" href="index.php">Acceuil</a>
         <button class="navbar-toggler hvr-grow" type="button" data-toggle="collapse" data-target="#navbarSupportedContent"
             aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
             <span class="navbar-toggler-icon"></span>
@@ -29,20 +29,20 @@
 
         <div class="collapse navbar-collapse" id="navbarSupportedContent">
             <ul class="navbar-nav mr-auto font-weight-bold">
-                <li class="nav-item">
-                    <a class="nav-link text-white hvr-grow" href="enregistrement.php">Enregistrement</a>
+                <!-- <li class="nav-item">
+                    <a class="nav-link hvr-grow" href="enregistrement.php">Enregistrement</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white hvr-grow" href="connexion.php">Connexion</a>
+                    <a class="nav-link hvr-grow" href="connexion.php">Connexion</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white hvr-grow" href="Utilisateurs.php">Utilisateurs</a>
+                    <a class="nav-link hvr-grow" href="Utilisateurs.php">Utilisateurs</a>
                 </li>
                 <li class="nav-item">
-                    <a class="nav-link text-white hvr-grow" href="calculateurDeCharge.php">Calculateur de charges</a>
-                </li>
+                    <a class="nav-link hvr-grow" href="calculateurDeCharge.php">Calculateur de charges</a>
+                </li> -->
                 <li class="nav-item">
-                    <a class="nav-link text-white hvr-grow" href="maps.php">Map</a>
+                    <a class="nav-link hvr-grow" href="maps.php">Map</a>
                 </li>
 
             </ul>
@@ -53,13 +53,17 @@
         </div>
     </nav>
 
-    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/popper.js@1.14.7/dist/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.3.1/dist/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
 <script src="https://unpkg.com/aos@next/dist/aos.js"></script>
   <script>
     AOS.init();
+    
   </script>
+  <script src="./assets/js/choreographer.min.js"></script>
+  <script src="./assets/js/anime.min.js"></script>
 </body>
 
 </html>
